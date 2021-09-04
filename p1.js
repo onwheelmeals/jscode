84289d56d9fb26a8d06971dc2a11b528795458c3
onwheelmeals <onwheelmeals@gmail.com>
2021-09-04 08:24:25 +0000

    SetTimeout Functionality

--- p1.js
+++ p1.js
@@ -1,1 +1,9 @@
-hii
+// Set timeout Problem which will print 1st
+console.log("hii this is print 1");
+
+setTimeout(() => {
+  console.log("hii this is setTimeout fn 1")
+}, 0);
+
+console.log("hii this is print 2");
+
